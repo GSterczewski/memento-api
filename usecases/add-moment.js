f29c3813416d07c4f5911f28ab4e85aa5c292d61
g.sterczewski <sterczewski.dev@protonmail.ch>
2020-04-02 08:37:13 +0200

    Add usecase for creating and adding new moment to database

---
+++ usecases/add-moment.js
@@ -0,0 +1,20 @@
+//const buildMoment = require("../moment/moment")
+const buildAddMoment = ({
+    momentsDB,
+    buildMoment
+}) => (momentInfo) => {
+    const moment = buildMoment(momentInfo)
+    return momentsDB.insert({
+        owner: moment.getOwner(),
+        name: moment.getName(),
+        createdOn: moment.getCreatedOn(),
+        modifiedOn: moment.getModifiedOn(),
+        date: moment.getDate(),
+        tags: moment.getTags(),
+        source: moment.getSource(),
+        isFavourite: moment.getIsFavourite(),
+        id: moment.getID()
+    })
+}
+
+module.exports = buildAddMoment
