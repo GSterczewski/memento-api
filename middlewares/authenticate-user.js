7553dc92246e2197bd6e45d84dd26cbe7b55e29c
g.sterczewski <sterczewski.dev@protonmail.ch>
2020-04-16 10:39:20 +0200

    Change auth middleware to use jwt

--- middlewares/authenticate-user.js
+++ middlewares/authenticate-user.js
@@ -2,12 +2,30 @@ const {
     URLS,
     ErrorTypes
 } = require("../config")
-module.exports = (req, res, next) => {
-    let authenticated = true
-    if (authenticated) {
-        next()
-    } else {
-        next(ErrorTypes.auth)
+const tokens = require("../lib/token");
+const verifyAuthHeader = (req) => {
+    if (!req.headers.authorization) {
+        return false
     }
+    let headers = req.headers.authorization.split(" ")
+    if (headers[0] !== 'Bearer') {
+        return false
+    }
+    return true
+}
+const verifyToken = req => tokens.verifyToken({
+    token: req.headers.authorization.split(" ")[1]
+})
+module.exports = async (req, res, next) => {
+    //check for auth header
+    try {
 
+        if (!(verifyAuthHeader(req) && await verifyToken(req))) {
+            return next(ErrorTypes.auth)
+        }
+    } catch (err) {
+
+        return next(ErrorTypes.auth)
+    }
+    next()
 }
